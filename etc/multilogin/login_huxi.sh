4944f858fbc2082bbec3d984bffafd5586f7917b
Zesuy Vortex <hongri580@163.com>
2026-03-02 21:09:51 +0800

    update shell

--- etc/multilogin/login_huxi.sh
+++ etc/multilogin/login_huxi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# 虎溪模板 (创建者: Zesuy)
+# 虎溪模板 (创建者: Zesuy) 
 # 解析命令行参数
 INTERFACE=""
 WLAN_USER_ACCOUNT=""
@@ -62,14 +62,13 @@ if [ -z "$INTERFACE" ] || [ -z "$WLAN_USER_ACCOUNT" ] || [ -z "$WLAN_USER_PASSWO
 fi
 
 # 通过逻辑接口名获取物理接口名
-
 PHYSICAL_INTERFACE=$(/sbin/uci get network.$INTERFACE.device)
 if [ -z "$PHYSICAL_INTERFACE" ]; then
-    # 只有当变量真的为空时，才认为是失败
     log 2 "错误: 无法通过uci获取接口 '$INTERFACE' 的物理设备名称 (device)"
     exit 5
 fi
 log 0 "调试: 逻辑接口 '$INTERFACE' 对应的物理接口是 '$PHYSICAL_INTERFACE'"
+
 # 获取当前的 MAC 地址和 IP 地址
 WLAN_USER_MAC=$(cat /sys/class/net/$PHYSICAL_INTERFACE/address)
 WLAN_USER_IP=$(ifconfig $PHYSICAL_INTERFACE | grep 'inet ' | awk '{print $2}' | sed 's/addr://')
@@ -79,13 +78,14 @@ if [ -z "$WLAN_USER_IP" ]; then
     exit 6
 fi
 
-# 定义编码后的UA参数
-PC_UA="Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F134.0.0.0%20Safari%2F537.36"
-MOBILE_UA="Mozilla%2F5.0%20%28Linux%3B%20U%3B%20Android%2011%3B%20zh-cn%3B%20MI%209%20Build%2FRKQ1.200826.002%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20likeGecko%29%20Version%2F4.0%20Mobile%20Safari%2F537.36%20MQQBrowser%2F11.9"
+# 定义更新后的编码 UA 参数
+PC_UA="Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F144.0.0.0%20Safari%2F537.36%20Edg%2F144.0.0.0"
+MOBILE_UA="Mozilla%2F5.0%20(Linux%3B%20Android%208.0.0%3B%20SM-G955U%20Build%2FR16NW)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F144.0.0.0%20Mobile%20Safari%2F537.36%20Edg%2F144.0.0.0"
 
 # 检查当前认证状态的函数
 check_status() {
-    local status_url="http://10.254.7.4/drcom/chkstatus?callback=dr1002&jsVersion=4.X&v=5505&lang=zh"
+    # 状态接口域名同步更新为 login.cqu.edu.cn
+    local status_url="http://login.cqu.edu.cn/drcom/chkstatus?callback=dr1002&jsVersion=4.X&v=5505&lang=zh"
     local response=$(mwan3 use $INTERFACE curl -s "$status_url")
     
     # 提取JSON中的result字段
@@ -99,20 +99,20 @@ check_status() {
         return 1
     else
         log 2 "状态检查失败，响应: $response"
-        return 2
+        return 1
     fi
 }
 
 # 执行登录函数
 do_login() {
-    # 根据UA类型选择URL和参数
+    # 根据UA类型选择最新的URL和参数 (注意: 域名、term_ua、MAC地址全零以及版本号等已更新)
     if [ "$UA_TYPE" = "pc" ]; then
-        local LOGIN_URL="http://10.254.7.4:801/eportal/portal/login?callback=dr1004&login_method=1&user_account=%2C0%2C$WLAN_USER_ACCOUNT&user_password=$WLAN_USER_PASSWORD&wlan_user_ip=$WLAN_USER_IP&wlan_user_ipv6=&wlan_user_mac=$WLAN_USER_MAC&wlan_ac_ip=&wlan_ac_name=&ua=$PC_UA&term_type=1&jsVersion=4.2&terminal_type=1&lang=zh-cn&v=9875&lang=zh"
+        local LOGIN_URL="http://login.cqu.edu.cn:801/eportal/portal/login?callback=dr1004&login_method=1&user_account=%2C0%2C$WLAN_USER_ACCOUNT&user_password=$WLAN_USER_PASSWORD&wlan_user_ip=$WLAN_USER_IP&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&term_ua=$PC_UA&term_type=1&jsVersion=4.2.2&terminal_type=1&lang=zh-cn&v=1176&lang=zh-cn"
     else
-        local LOGIN_URL="http://10.254.7.4:801/eportal/portal/login?callback=dr1005&login_method=1&user_account=%2C1%2C$WLAN_USER_ACCOUNT&user_password=$WLAN_USER_PASSWORD&wlan_user_ip=$WLAN_USER_IP&wlan_user_ipv6=&wlan_user_mac=$WLAN_USER_MAC&wlan_ac_ip=&wlan_ac_name=&ua=$MOBILE_UA&term_type=2&jsVersion=4.2&terminal_type=2&lang=zh-cn&v=7090&lang=zh"
+        local LOGIN_URL="http://login.cqu.edu.cn:801/eportal/portal/login?callback=dr1005&login_method=1&user_account=%2C1%2C$WLAN_USER_ACCOUNT&user_password=$WLAN_USER_PASSWORD&wlan_user_ip=$WLAN_USER_IP&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&term_ua=$MOBILE_UA&term_type=2&jsVersion=4.2.2&terminal_type=2&lang=zh-cn&v=1176&lang=zh-cn"
     fi
     
-    log 1 "尝试登录 ($UA_TYPE UA)，使用IP: $WLAN_USER_IP, MAC: $WLAN_USER_MAC"
+    log 1 "尝试登录 ($UA_TYPE UA)，使用IP: $WLAN_USER_IP"
     local response=$(mwan3 use $INTERFACE curl -s "$LOGIN_URL")
     
     local json_response=$(echo "$response" | grep -o '{.*}')
